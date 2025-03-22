5485ca12b309a0f8b1947c62d4650cb251e0116a
Michael LapTop <kir.commersion@internet.ru>
2025-03-22 14:39:46 +0300

    added mocks go song_gateway.go

--- internal/gateway/song_gateway.go
+++ internal/gateway/song_gateway.go
@@ -2,6 +2,7 @@ package gateway
 
 import "Music-library/internal/models"
 
+//go:generate mockgen -source=song_gateway.go -destination=mocks/mock_gateway.go -package=mocks
 type SongGateway interface {
 	GetSongs(filter map[string]string, limit, offset int) ([]models.Song, error)
 	GetSongByID(id uint) (*models.Song, error)
